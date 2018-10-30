6603a261ab996a1253c800733ffb3108a05726ae
Greg Morgan <gregm9999@gmail.com>
2018-10-30 02:19:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  # def initialize(name)
+  #   @name=name
+  # end
+  def name=(name)
+    @name=name
+  end
+  def name
+    "#{@name}".strip
+  end
+
+  def breed=(breed)
+    @breed=breed
+  end
+  def breed
+    "#{@breed}".strip
+  end
+end

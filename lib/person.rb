64003721458ec7d3cc7f409fd7f1534678e90233
Greg Morgan <gregm9999@gmail.com>
2018-10-30 00:15:04 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def initialize(name)
+    @name=name
+  end
+  def name=(name)
+    "#{name}"
+  end
+  def initizlize(job)
+    @job=job
+  end
+  def job=(job)
+    "#{job}"
+  end
+end
